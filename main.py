ccb260e8d8d2aca28b905747263f0c2db519b0ac
therealmarvin <nakedtoothbrush@hotmail.com>
2020-09-13 15:13:49 -0400

    code cleanup

--- main.py
+++ main.py
@@ -6,16 +6,6 @@ import os
 import glob
 
 
-def get_page(url):
-    import urllib3
-    http = urllib3.PoolManager()
-    r = http.request('GET', url)
-    print((r.status))
-    print((r.data))
-
-    return None
-
-
 def get_video_name(video_folder):
     filelist = glob.glob(os.path.join(video_folder, '*.mp4'))
     return filelist
@@ -29,10 +19,6 @@ def path_leaf(path):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    url = 'https://sitescours.monportail.ulaval.ca/ena/site/module?editionModule=false&idSite=121706&idPage=2790239&idModule=1030262&_js=true'
-    # get_page(url)
-
-    #video = "C:/Users/naked_000/Desktop/video(1).mp4"
     out_dir = "./out"
 
     videos = get_video_name("videos")
